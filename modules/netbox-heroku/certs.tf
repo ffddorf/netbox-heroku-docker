c2d1303d351daa4407b17c7a538661aae5a3df37
Marcus Weiner <marcus.weiner@gmail.com>
2021-04-03 16:38:18 +0200

    Provision cloudflare origin cert (#6)

---
+++ modules/netbox-heroku/certs.tf
@@ -0,0 +1,32 @@
+variable "ssl_certificate_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "ssl_certificate_pem" {
+  type    = string
+  default = ""
+}
+
+variable "ssl_private_key_pem" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+resource "heroku_addon" "ssl" {
+  count = var.ssl_certificate_enabled ? 1 : 0
+
+  app  = heroku_app.netbox.id
+  plan = "ssl"
+}
+
+# Establish certificate for a given application
+resource "heroku_cert" "custom_cert" {
+  count = var.ssl_certificate_enabled ? 1 : 0
+
+  app               = heroku_app.netbox.id
+  certificate_chain = var.ssl_certificate_pem
+  private_key       = var.ssl_private_key_pem
+  depends_on        = [heroku_addon.ssl]
+}

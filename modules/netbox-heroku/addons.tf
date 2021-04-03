2a91d57b3e1e2a9ba3482ad06789de56c7cc472f
Marcus Weiner <marcus.weiner@gmail.com>
2021-04-03 18:45:42 +0200

    Prevent destruction of stateful addons

--- modules/netbox-heroku/addons.tf
+++ modules/netbox-heroku/addons.tf
@@ -1,9 +1,17 @@
 resource "heroku_addon" "postgres" {
   app  = heroku_app.netbox.id
   plan = "heroku-postgresql:hobby-dev"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "heroku_addon" "redis" {
   app  = heroku_app.netbox.id
   plan = "heroku-redis:hobby-dev"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }

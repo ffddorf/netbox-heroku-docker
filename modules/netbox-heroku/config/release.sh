1deb23da8f5b3145d2882beb03e39934fbf607a3
Marcus Weiner <marcus.weiner@gmail.com>
2023-11-25 23:57:28 +0100

    Fix scripts to work with newer release

--- modules/netbox-heroku/config/release.sh
+++ modules/netbox-heroku/config/release.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Stop when an error occurs
 set -e
 
-exec /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py migrate
+source /opt/netbox/venv/bin/activate
+
+echo "⚙️ Applying database migrations"
+./manage.py migrate --no-input
+echo "⚙️ Running trace_paths"
+./manage.py trace_paths --no-input
+echo "⚙️ Removing stale content types"
+./manage.py remove_stale_contenttypes --no-input
+echo "⚙️ Removing expired user sessions"
+./manage.py clearsessions
+echo "⚙️ Building search index (lazy)"
+./manage.py reindex --lazy

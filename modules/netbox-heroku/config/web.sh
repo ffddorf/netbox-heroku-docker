f8af611e5a0ce833a97c3b489fee49508db0ff4d
Marcus Weiner <marcus.weiner@gmail.com>
2023-11-26 01:23:48 +0100

    Fix setup to work with newer release (#79)
    * Fix scripts to work with newer release
    
    * Fix openapi spec generation
    
    * Improve script execution
    
    * Patch nginx config to work with Heroku

--- modules/netbox-heroku/config/web.sh
+++ modules/netbox-heroku/config/web.sh
@@ -1,80 +1,17 @@
 #!/bin/bash
 
-# Adapted from netbox-docker/docker/launch-netbox.sh
+set -e -o pipefail
 
-UNIT_CONFIG="${UNIT_CONFIG-/etc/unit/nginx-unit.json}"
-UNIT_SOCKET="/opt/unit/unit.sock"
-
-load_configuration() {
-  MAX_WAIT=10
-  WAIT_COUNT=0
-  while [ ! -S $UNIT_SOCKET ]; do
-    if [ $WAIT_COUNT -ge $MAX_WAIT ]; then
-      echo "⚠️ No control socket found; configuration will not be loaded."
-      return 1
-    fi
-
-    WAIT_COUNT=$((WAIT_COUNT + 1))
-    echo "⏳ Waiting for control socket to be created... (${WAIT_COUNT}/${MAX_WAIT})"
-
-    sleep 1
-  done
-
-  # even when the control socket exists, it does not mean unit has finished initialisation
-  # this curl call will get a reply once unit is fully launched
-  curl --silent --output /dev/null --request GET --unix-socket $UNIT_SOCKET http://localhost/
-
-  echo "⚙️ Applying configuration from $UNIT_CONFIG"
-
-  RESP_CODE=$(
-    curl \
-      --silent \
-      --output /dev/null \
-      --write-out '%{http_code}' \
-      --request PUT \
-      --data-binary "@${UNIT_CONFIG}" \
-      --unix-socket $UNIT_SOCKET \
-      http://localhost/config
-  )
-  if [ "$RESP_CODE" != "200" ]; then
-    echo "⚠️ Could not load Unit configuration, status $RESP_CODE"
-    kill "$(cat /opt/unit/unit.pid)"
-    return 1
-  fi
-
-  echo "⚙️ Applying Heroku listener config"
-
-  RESP_CODE=$(
-    curl \
-      --silent \
-      --output /dev/null \
-      --write-out '%{http_code}' \
-      --request PUT \
-      --data "{\"*:${PORT}\": {\"pass\": \"routes/main\"}}" \
-      --unix-socket $UNIT_SOCKET \
-      http://localhost/config/listeners
-  )
-  if [ "$RESP_CODE" != "200" ]; then
-    echo "⚠️ Could not load Unit configuration, status $RESP_CODE"
-    kill "$(cat /opt/unit/unit.pid)"
-    return 1
-  fi
-
-  echo "✅ Unit configuration loaded successfully"
-}
-
-# Gather openapi spec
-python manage.py generate_swagger static/openapi.json
-
-load_configuration &
+# Generate static openapi spec
+python manage.py spectacular --file static/openapi.json
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-exec unitd \
-  --no-daemon \
-  --control unix:$UNIT_SOCKET \
-  --pid /opt/unit/unit.pid \
-  --log /dev/stdout \
-  --state /opt/unit/state/ \
-  --tmp /opt/unit/tmp/
+# Patch nginx unit config to use Heroku port
+jq '. + { listeners: { "*:\(env.PORT)": { pass: "routes/main" } } }' \
+  < /etc/unit/nginx-unit.json \
+  > /etc/unit/nginx-unit-heroku.json
+export UNIT_CONFIG=/etc/unit/nginx-unit-heroku.json
+
+/opt/netbox/launch-netbox.sh

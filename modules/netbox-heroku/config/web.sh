73c2282b055889d091fdb7d3915643b6f3feae90
Marcus Weiner <marcus.weiner@gmail.com>
2023-11-26 01:21:49 +0100

    Patch nginx config to work with Heroku

--- modules/netbox-heroku/config/web.sh
+++ modules/netbox-heroku/config/web.sh
@@ -8,4 +8,10 @@ python manage.py spectacular --file static/openapi.json
 # Collect static files
 python manage.py collectstatic --noinput
 
+# Patch nginx unit config to use Heroku port
+jq '. + { listeners: { "*:\(env.PORT)": { pass: "routes/main" } } }' \
+  < /etc/unit/nginx-unit.json \
+  > /etc/unit/nginx-unit-heroku.json
+export UNIT_CONFIG=/etc/unit/nginx-unit-heroku.json
+
 /opt/netbox/launch-netbox.sh
